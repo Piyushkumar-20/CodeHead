35c9316c6580d3f77e2f8232373ff499ce1751de
Piyushkumar-20 <pk6686742@gmail.com>
2026-06-18 15:22:58 +0530

    feat: Implement authentication and dashboard layouts
    - Added proxy functionality in proxy.ts to handle authentication redirection.
    - Introduced protected and auth layouts in app/(protected)/layout.tsx and app/(auth)/layout.tsx for session management.
    - Created dashboard components including DashboardShell, DashboardNav, and DashboardSidebar for user navigation.
    - Developed user menu component for user interactions in features/auth/components/user-menu.tsx.
    - Updated routing and navigation structure with DASHBOARD_ROUTES in lib/routes.ts.

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,17 @@
+import {requireAuth} from "@/features/auth/action"
+import React from "react"
+
+export default async function ProtectedLayout({
+    children
+}:{
+    children:React.ReactNode
+}){
+    await requireAuth();
+
+    return (
+        <div className="min-h-svh">
+            {children}
+        </div>
+    )
+
+}
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { createAuthClient } from "better-auth/client"
-const authClient =  createAuthClient()
+import { createAuthClient } from "better-auth/react"
+export const authClient =  createAuthClient()
 
 const signIn = async () => {
     const data = await authClient.signIn.social({
